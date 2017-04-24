97c0f0f62059448762f82065cf40ac3bcd837e6b
joshruane98 <joshuaruane@gmail.com>
2017-04-24 14:21:22 +0100

    Add files via upload

---
+++ StarshipFontana/src/SFApp.cpp
@@ -0,0 +1,165 @@
+#include "SFApp.h"
+
+SFApp::SFApp(std::shared_ptr<SFWindow> window) : fire(0), is_running(true), sf_window(window) {
+  int canvas_w, canvas_h;
+  SDL_GetRendererOutputSize(sf_window->getRenderer(), &canvas_w, &canvas_h);
+
+  app_box = make_shared<SFBoundingBox>(Vector2(canvas_w, canvas_h), canvas_w, canvas_h);
+  score = 0;
+  player  = make_shared<SFAsset>(SFASSET_PLAYER, sf_window);
+  auto player_pos = Point2(canvas_w/2, 22);
+  player->SetPosition(player_pos);
+
+  const int number_of_aliens = 10;
+  for(int i=0; i<number_of_aliens; i++) {
+    // place an alien at width/number_of_aliens * i
+    auto alien = make_shared<SFAsset>(SFASSET_ALIEN, sf_window);
+    auto pos   = Point2((canvas_w/number_of_aliens) * i, 200.0f);
+    alien->SetPosition(pos);
+    aliens.push_back(alien);
+  }
+
+  auto coin = make_shared<SFAsset>(SFASSET_COIN, sf_window);
+  auto coin_pos  = Point2((canvas_w/4), 100);
+  coin->SetPosition(coin_pos);
+  coins.push_back(coin);
+
+  auto wall1 = make_shared<SFAsset>(SFASSET_WALL, sf_window);
+  auto wall1_pos = Point2(32.0f, 16.0f);
+  wall1->SetPosition(wall1_pos);
+  walls.push_back(wall1);
+}
+
+SFApp::~SFApp() {
+}
+
+/**
+ * Handle all events that come from SDL.
+ * These are timer or keyboard events.
+ */
+void SFApp::OnEvent(SFEvent& event) {
+  SFEVENT the_event = event.GetCode();
+  switch (the_event) {
+  case SFEVENT_QUIT:
+    is_running = false;
+    break;
+  case SFEVENT_UPDATE:
+    OnUpdateWorld();
+    OnRender();
+    break;
+  case SFEVENT_PLAYER_LEFT:
+    player->GoWest();
+    break;
+  case SFEVENT_PLAYER_RIGHT:
+    player->GoEast();
+    break;
+  case SFEVENT_PLAYER_UP:
+    player->GoNorth();
+    break;
+  case SFEVENT_PLAYER_DOWN:
+    player->GoSouth();
+    break;
+  case SFEVENT_FIRE:
+    fire ++;
+    FireProjectile();
+    break;
+  }
+}
+
+int SFApp::OnExecute() {
+  // Execute the app
+  SDL_Event event;
+  while (SDL_WaitEvent(&event) && is_running) {
+    // wrap an SDL_Event with our SFEvent
+    SFEvent sfevent((const SDL_Event) event);
+    // handle our SFEvent
+    OnEvent(sfevent);
+  }
+}
+
+void SFApp::OnUpdateWorld() {
+  // Update projectile positions
+  for(auto p: projectiles) {
+    p->GoNorth();
+  }
+
+  for(auto c: coins) {
+    c->GoNorth();
+  }
+
+  // Update enemy positions
+  for(auto a : aliens) {
+    // do something here
+  }
+
+  // Detect collisions
+  for(auto p : projectiles) {
+    for(auto a : aliens) {
+      if(p->CollidesWith(a)) {
+        p->HandleCollision();
+        a->HandleCollision();
+	score++;
+      }
+    }
+  }
+
+    for(auto w : walls) {
+      if(player->CollidesWith(w)) {
+	player->HandleCollision();
+	w->HandleCollision();
+      }
+    }
+
+    for(auto c : coins) {
+      if(player->CollidesWith(c)) {
+	c->HandleCollision();
+	score += 100;
+      }
+    }
+
+  // remove dead aliens (the long way)
+  list<shared_ptr<SFAsset>> tmp;
+  for(auto a : aliens) {
+    if(a->IsAlive()) {
+      tmp.push_back(a);
+    }
+  }
+  aliens.clear();
+  aliens = list<shared_ptr<SFAsset>>(tmp);
+}
+
+void SFApp::OnRender() {
+  SDL_RenderClear(sf_window->getRenderer());
+
+  // draw the player
+  if(player->IsAlive()) {player->OnRender();}
+  else if(player->IsAlive()==SFASSET_DEAD) {
+    cout << score;
+  }
+
+  for(auto p: projectiles) {
+    if(p->IsAlive()) {p->OnRender();}
+  }
+
+  for(auto a: aliens) {
+    if(a->IsAlive()) {a->OnRender();}
+  }
+
+  for(auto c: coins) {
+    if(c->IsAlive()) { c->OnRender();}
+  }
+
+  for(auto w: walls) {
+    w->OnRender();
+  }
+
+  // Switch the off-screen buffer to be on-screen
+  SDL_RenderPresent(sf_window->getRenderer());
+}
+
+void SFApp::FireProjectile() {
+  auto pb = make_shared<SFAsset>(SFASSET_PROJECTILE, sf_window);
+  auto v  = player->GetPosition();
+  pb->SetPosition(v);
+  projectiles.push_back(pb);
+}
